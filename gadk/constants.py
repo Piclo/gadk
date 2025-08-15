56473ed473f56f8a94f3531408f9a74322d51875
Josh Wainwright <josh@piclo.energy>
2025-08-15 14:21:33 +0100

    Update 3rd party actions to latest versions

--- gadk/constants.py
+++ gadk/constants.py
@@ -1,5 +1,5 @@
 __all__ = ["ACTION_CHECKOUT", "ACTION_DOWNLOAD", "ACTION_UPLOAD"]
 
-ACTION_CHECKOUT = "actions/checkout@v4"
-ACTION_DOWNLOAD = "actions/download-artifact@v4"
+ACTION_CHECKOUT = "actions/checkout@v5"
+ACTION_DOWNLOAD = "actions/download-artifact@v5"
 ACTION_UPLOAD = "actions/upload-artifact@v4"
